15dee9562e41f2906c639db2a96c6e4717a7d203
mahima <mahipandey2206@gmail.com>
2025-05-19 22:05:03 +0530

    added product

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import Footer from './Components/Footer/Footer';
 import men_banner from './Components/assets/banner_mens.png'
 import women_banner from './Components/assets/banner_women.png'
 import kid_banner from './Components/assets/banner_kids.png'
+import LoginSignup from './Pages/LoginSignup';
+
 
 
 function App() {
@@ -27,7 +29,7 @@ function App() {
              </Route>
 
              <Route path='/cart' element={<Cart/>}/>
-             <Route path='./login' element={<loginSignup/>}/>
+             <Route path='/login' element={<LoginSignup/>}/>
             
              
 
---
+++ src/Components/ProductDisplay/ProductDisplay.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './ProductDisplay.css' 
+
+const ProductDisplay = (props) => {
+    const {product} = props; 
+  return (
+    <div className='productDisplay'>
+        <div className="productdisplay-left">
+            <div className="productdisplay-img-list">
+                <img src={product.image} alt=""/>
+                <img src={product.image} alt=""/>
+                <img src={product.image} alt=""/>
+                <img src={product.image} alt=""/>
+                  
+
+            </div>
+
+            
+
+
+        </div>
+        <div className="productdisplay-right"></div>
+
+
+
+      
+    </div>
+  );
+};
+
+export default ProductDisplay;
